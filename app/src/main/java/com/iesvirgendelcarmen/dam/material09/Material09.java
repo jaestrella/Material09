af16e07ae84478d948502a9ccc2a7808fea37636
jaestrella <jaestrella1991@gmail.com>
2017-11-29 12:22:18 +0100

    sin acabar

--- app/src/main/java/com/iesvirgendelcarmen/dam/material09/Material09.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/material09/Material09.java
@@ -4,14 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Material09 extends AppCompatActivity {
     Toolbar toolbar;
+    ImageView icono;
+    private static  final int DURACION=250;
+    boolean giro=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material09);
+
+        icono=(ImageView)findViewById(R.id.imagen2);
 
         toolbar=(Toolbar)findViewById(R.id.barra1);
         toolbar.setTitle("EL RAPTO DE LAS SABINAS");
@@ -35,5 +44,24 @@ public class Material09 extends AppCompatActivity {
                 return true;
             }
         });
+
+
+    }
+    private void rotate(float angulo){
+        Animation animacion=new RotateAnimation(0.0f,angulo,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        animacion.setFillAfter(true);
+        animacion.setDuration(DURACION);
+        icono.startAnimation(animacion);
+    }
+    public void tocado(View view){
+        if (giro!=false){
+            icono.setImageResource(R.mipmap.menos);
+            rotate(-180.0f);
+            giro=false;
+        }else{
+            icono.setImageResource(R.mipmap.mas);
+            rotate(180.0f);
+            giro=true;
+        }
     }
 }
